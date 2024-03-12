2b67a178f9a773d77003e3eb8678e566f74b2e56
RithikKalra <71096898+RithikKalra@users.noreply.github.com>
2024-03-12 01:17:10 -0400

    Upload Project to Github

---
+++ README.md
@@ -0,0 +1,19 @@
+# COVID-19 Detector
+Live: https://covid-19-xray-diagnose.herokuapp.com/
+
+A web application that uses machine learning to determine whether a user has tested positive or negative for COVID-19 based on a chest X-ray image.
+
+This was done by using Tensorflow to develop a Convolutional Neural Network in Python that classified images as "positive" or "negative" with a 97% accuracy. The Tensorflow model was trained using the COVID-19 Radiography Database dataset found here on Kaggle: https://www.kaggle.com/tawsifurrahman/covid19-radiography-database
+
+Frontend:
+- HTML
+- CSS
+- JavaScript
+- Bootstrap
+
+Backend:
+- Python
+- Flask
+
+## License
+[MIT](https://raw.githubusercontent.com/nelavensubas/COVID-19-Detector/main/LICENSE)
---
+++ static/js/Covid-19SeftTest.js
@@ -0,0 +1,31 @@
+//Determined from Machine Learning
+//testResult = 0;
+console.log('***in js')  //for debugging
+function displayResult(){
+
+    let progressBar = document.getElementById("progBar");
+            document.getElementById("pBar").style.display = "block"; 
+
+            let i = 2
+            function progress(){
+                if(i <100){
+                    i++;
+                    progressBar.style.width = i + "%";
+                }
+                setTimeout("progress()", 100);
+            }
+            progress();
+
+    if(testResult == 1){
+        document.getElementById("pResult").style.display = "block";
+    }
+    else{
+        document.getElementById("nResult").style.display = "block";
+    }
+}
+
+let el = document.getElementById("file");
+el.addEventListener('input', displayResult);
+
+
+
